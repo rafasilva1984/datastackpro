dd458d3587989de1f652ee0e583a30e147b5940d
rafasilva1984 <rafaelldasilva1984@gmail.com>
2025-08-15 10:25:44 -0300

    Delete elasticsearch/poc-cicd-jenkins-kind-elastic directory

--- elasticsearch/poc-cicd-jenkins-kind-elastic/prod/ci/trivy-scan.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IMAGE="$1"
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock aquasec/trivy:0.54.1   image --exit-code 1 --severity CRITICAL,HIGH "$IMAGE"
