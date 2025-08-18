4a235963d8223e7694238511f9e7c014460142a9
rafasilva1984 <rafaelldasilva1984@gmail.com>
2025-08-18 13:35:29 -0300

    Delete elasticsearch/observabilidade-experiencia-negocio directory

--- elasticsearch/observabilidade-experiencia-negocio/scripts/load_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "🚀 Carregando estrutura e dados..."
-# Aqui você incluiria os scripts de ingestão e dashboards
--- elasticsearch/observabilidade-experiencia-negocio/scripts/setup_token.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🔑 Gerando Service Account Token para o Kibana..."
-
-TOKEN=$(docker exec es-obs-exp-neg   bin/elasticsearch-service-tokens create elastic/kibana kb-token   | awk '/value/ {print $3}')
-
-echo "KIBANA_SERVICE_ACCOUNT_TOKEN=$TOKEN" >> .env
-echo "✅ Token salvo no .env"
